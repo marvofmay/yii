94ecb55bd6973e59389f6cafff13d4ed8db5c858
marcin <marcin.jaroszynski.gdansk@gmail.com>
2023-12-13 18:58:51 +0100

    refactor - EmailService

---
+++ service-salestracker-user/src/salestracker/protected/helpers/EmailService.php
@@ -0,0 +1,23 @@
+<?php
+
+class EmailService
+{
+    public function __construct (
+        private readonly string $email,
+        private readonly string $viewName,
+        private readonly array $viewParams
+    ) { }
+    public function sendEmail(): self
+    {
+        $from = Yii::app()->params['adminEmail'];
+        $mail = new YiiMailer($this->viewName, $this->viewParams);
+        $mail->setFrom($from, $from);
+        $mail->setSubject('E-mail confirming registration.');
+        $mail->setTo($this->email);
+        if (! $mail->send()) {
+            throw new Exception(sprintf('Error while sending email: %s', $mail->getError()));
+        }
+
+        return $this;
+    }
+}
