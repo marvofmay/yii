ff9efe0db3ed6a68d644ce55c38bcbeee3e6536e
marcin <marcin.jaroszynski.gdansk@gmail.com>
2023-12-13 19:25:00 +0100

    refactor - use UserService

--- service-salestracker-user/src/salestracker/protected/controllers/user/RegisterAction.php
+++ service-salestracker-user/src/salestracker/protected/controllers/user/RegisterAction.php
@@ -13,16 +13,8 @@ class RegisterAction extends CAction
                     if ($existingUser !== null) {
                         $model->addError('email', 'This email already exists in the database.');
                     } else {
-                          $userService = new UserService(
-                              null,
-                              null,
-                              $model->email,
-                              null,
-                              $model->password,
-                              date('Y-m-d'),
-                              date('Y-m-d H:i:s')
-                          );
-                          $userService->setUser()->saveUserInDB();
+                        $userService = new UserService(null, null, $model->email, null, $model->password, date('Y-m-d'), date('Y-m-d H:i:s'));
+                        $userService->setUser()->saveUserInDB();
                         Yii::app()->user->setFlash('register', 'Thank you for your registration. You can log in now.');
                     }
                 }
